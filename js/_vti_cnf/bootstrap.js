6d762cb515e9987d21f4d2aeb361e7830462c80b
GashWare <37035042+GashWare@users.noreply.github.com>
2025-11-22 20:30:31 -0800

    Upload2
    Upload2

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Apr 2021 20:38:06 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|oconnerbrotherswelding/about.html oconnerbrotherswelding/service.html oconnerbrotherswelding/contact.html oconnerbrotherswelding/index.html oconnerbrotherswelding/team.html
+vti_author:SR|PC\\jgcpu
+vti_modifiedby:SR|PC\\jgcpu
+vti_timecreated:TR|27 Aug 2020 13:40:02 -0000
+vti_nexttolasttimemodified:TW|14 Apr 2021 20:36:59 -0000
+vti_cacheddtm:TX|14 Apr 2021 20:38:06 -0000
+vti_filesize:IR|141185
---
+++ js/_vti_cnf/custom.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Jun 2020 13:58:25 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|oconnerbrotherswelding/about.html oconnerbrotherswelding/service.html oconnerbrotherswelding/contact.html oconnerbrotherswelding/index.html oconnerbrotherswelding/team.html
